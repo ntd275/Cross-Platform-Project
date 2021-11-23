46e41c623f6c480fc67dde6069e37a870811704c
PhongHH <realphongha@gmail.com>
2021-11-23 10:58:31 +0700

    Change some message popups

--- src/screens/ProfileEditScreen.js
+++ src/screens/ProfileEditScreen.js
@@ -120,9 +120,28 @@ export default function ProfileEditScreen({ navigation }) {
                     context.dispatch({type: 'CHANGEUSERNAME', username: res.data.data.username});
                     appContext.setGender(res.data.data.gender);
                     appContext.setBirthday(res.data.data.birthday);
-                    Alert.alert("Thành công", "Đã cập nhật thông tin", [{ text: "OK" }]);
+                    appContext.displayMessage({
+                        message: "Đã cập nhật thông tin",
+                        type: "default",
+                        style: { width: 195, marginBottom: 200 },
+                        titleStyle: {fontSize: 14},
+                        duration: 1900,
+                        icon:"success",
+                        position: "center",
+                        backgroundColor: "#262626",
+                    });
+                    // Alert.alert("Thành công", "Đã cập nhật thông tin", [{ text: "OK" }]);
                 } else {
-                    Alert.alert("Thất bại", "Vui lòng nhập đầy đủ họ tên", [{ text: "OK" }]);
+                    appContext.displayMessage({
+                        message: "Vui lòng nhập đầy đủ họ tên",
+                        type: "default",
+                        style: { width: 195, marginBottom: 200 },
+                        titleStyle: { fontSize: 14 },
+                        duration: 1900,
+                        position: "center",
+                        backgroundColor: "#262626",
+                    });
+                    // Alert.alert("Thất bại", "Vui lòng nhập đầy đủ họ tên", [{ text: "OK" }]);
                 }
             } catch (err){
                 console.log(err)
