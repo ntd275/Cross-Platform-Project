98b3f312f1bfa7ffd9416ac2755a3f819d42edb4
Nguyễn Thế Tùng Dương <58430986+Duong991999@users.noreply.github.com>
2021-11-30 09:11:05 +0700

    Update LoiMoiKetBan.js

--- src/screens/LoiMoiKetBan.js
+++ src/screens/LoiMoiKetBan.js
@@ -2,7 +2,7 @@
 // import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useLayoutEffect } from "react";
 import {
   TextInput,
   ScrollView,
@@ -33,87 +33,124 @@ import VideoIcon from "../../assets/ic_video_line_24.svg";
 import CallIcon from "../../assets/ic_call_line_24.svg";
 import { Avatar } from "native-base";
 import { flex, flexDirection } from 'styled-system';
+import { BaseURL } from "../utils/Constants";
 
 
 
+export default function App({navigation}) {
+  const context = React.useContext(AuthContext);
+  const [ListS, setListS] = useState([]);
+  const [ListR, setListR] = useState([]);
+  const getListS = async () => {
+    try {
+      const accessToken = context.loginState.accessToken;
+      let List = await Api.getListFriendsRequest(accessToken);
+      // console.log(friends.data.data.friends);
+      // console.log(typeof friends.data.data.friends);
+      setListS(Object.values(List.data.data.sentList));
+    } catch (e) {
+      console.log(e);
+      navigation.navigate("NoConnectionScreen", {
+        message: "Vui lòng kiểm tra kết nối internet và thử lại",
+      });
+    }
+  };
+  useLayoutEffect(() => {
+    getListS();
+  }, []);
 
-export default function App() {
+  const getListR = async () => {
+    try {
+      const accessToken = context.loginState.accessToken;
+      let List = await Api.getListFriendsRequest(accessToken);
+      // console.log(friends.data.data.friends);
+      // console.log(typeof friends.data.data.friends);
+      setListR(Object.values(List.data.data.receivedList));
+    } catch (e) {
+      console.log(e);
+      navigation.navigate("NoConnectionScreen", {
+        message: "Vui lòng kiểm tra kết nối internet và thử lại",
+      });
+    }
+  };
+  useLayoutEffect(() => {
+    getListR();
+  }, []);
 
+//   const list1 = [
+//     {
+//       name: "Sơn Tùng MTP",
+//       img: "https://2sao.vietnamnetjsc.vn/images/2021/07/08/16/13/st.jpg",
+//     },
+//     {
+//       name: "Leo Messi",
+//       img: "https://scontent.fhan14-1.fna.fbcdn.net/v/t1.6435-9/199385759_345470910277839_3988273979229903886_n.jpg?_nc_cat=1&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=39BV_AnzYYgAX9HpGbf&_nc_ht=scontent.fhan14-1.fna&oh=202d27ef0fca02697c202aacd2c143e7&oe=61B3900F"
+//     },
+//     {
+//       name: "Cristiano Ronaldo",
+//       img: "https://scontent.fhan14-1.fna.fbcdn.net/v/t1.6435-9/190108336_322648402555040_2100790391455013605_n.jpg?_nc_cat=1&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=kXE0CTYzxPQAX9RZvZH&_nc_ht=scontent.fhan14-1.fna&oh=c5f2c24981e64650af49d1d6c965f433&oe=61B3ED00"
+//     },
+//     {
+//       name: "Trấn Thành",
+//       img: "https://scontent.fhan14-1.fna.fbcdn.net/v/t1.6435-9/180917342_344584213698521_5308351421019265373_n.jpg?_nc_cat=1&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=MhIQQu9YOUsAX-DttgX&_nc_ht=scontent.fhan14-1.fna&oh=74638f0ef922c05a1fd5476256b6d97e&oe=61B60A21"
+//     },
+//     {
+//       name: "Neymar Jr",
+//       img: "https://scontent.fhan14-1.fna.fbcdn.net/v/t1.6435-9/157299641_264410915245410_632132461500072208_n.jpg?_nc_cat=1&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=fJuLuDqZkpcAX-rzHT4&_nc_ht=scontent.fhan14-1.fna&oh=cf1898caee079102c344bd0fc66a5c90&oe=61B4DF6C"
+//     },
+//     {
+//       name: "Ngọc Trinh",
+//       img: "https://scontent.fhan14-1.fna.fbcdn.net/v/t1.6435-9/80806552_1802048246592648_5426944641696006144_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=ZoY3FDwVMFgAX_3xogw&_nc_oc=AQlQ9NoqYcARZl2x95KI2psmAPZs-vcLWDCF8CVqu3ifgcP9GxZM8g7-AQkKQGcK-QbtanvLi74ykV4NznNmVPf5&_nc_ht=scontent.fhan14-1.fna&oh=cf8c39e7fc855beb3b9f75ab79c6d08c&oe=61B23952"
+//     },
+//     {
+//       name: "Độ Mixi",
+//       img: "https://scontent.fhan14-2.fna.fbcdn.net/v/t1.6435-9/151296956_2198779476922608_9064820515933100826_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=174925&_nc_ohc=TzrZGvBVkBkAX_IeMqu&tn=d5LeRVdLTryOw5MC&_nc_ht=scontent.fhan14-2.fna&oh=438b541d1007f515e7cf69a82218b728&oe=61B4FBE2"
+//     },
+//     {
+//       name: "La Phu",
+//       img: "https://www.iptc.org/wp-content/uploads/2018/05/avatar-anonymous-300x300.png"
+//     },
 
-  const list1 = [
-    {
-      name: "Sơn Tùng MTP",
-      img: "https://2sao.vietnamnetjsc.vn/images/2021/07/08/16/13/st.jpg",
-    },
-    {
-      name: "Leo Messi",
-      img: "https://scontent.fhan14-1.fna.fbcdn.net/v/t1.6435-9/199385759_345470910277839_3988273979229903886_n.jpg?_nc_cat=1&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=39BV_AnzYYgAX9HpGbf&_nc_ht=scontent.fhan14-1.fna&oh=202d27ef0fca02697c202aacd2c143e7&oe=61B3900F"
-    },
-    {
-      name: "Cristiano Ronaldo",
-      img: "https://scontent.fhan14-1.fna.fbcdn.net/v/t1.6435-9/190108336_322648402555040_2100790391455013605_n.jpg?_nc_cat=1&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=kXE0CTYzxPQAX9RZvZH&_nc_ht=scontent.fhan14-1.fna&oh=c5f2c24981e64650af49d1d6c965f433&oe=61B3ED00"
-    },
-    {
-      name: "Trấn Thành",
-      img: "https://scontent.fhan14-1.fna.fbcdn.net/v/t1.6435-9/180917342_344584213698521_5308351421019265373_n.jpg?_nc_cat=1&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=MhIQQu9YOUsAX-DttgX&_nc_ht=scontent.fhan14-1.fna&oh=74638f0ef922c05a1fd5476256b6d97e&oe=61B60A21"
-    },
-    {
-      name: "Neymar Jr",
-      img: "https://scontent.fhan14-1.fna.fbcdn.net/v/t1.6435-9/157299641_264410915245410_632132461500072208_n.jpg?_nc_cat=1&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=fJuLuDqZkpcAX-rzHT4&_nc_ht=scontent.fhan14-1.fna&oh=cf1898caee079102c344bd0fc66a5c90&oe=61B4DF6C"
-    },
-    {
-      name: "Ngọc Trinh",
-      img: "https://scontent.fhan14-1.fna.fbcdn.net/v/t1.6435-9/80806552_1802048246592648_5426944641696006144_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=ZoY3FDwVMFgAX_3xogw&_nc_oc=AQlQ9NoqYcARZl2x95KI2psmAPZs-vcLWDCF8CVqu3ifgcP9GxZM8g7-AQkKQGcK-QbtanvLi74ykV4NznNmVPf5&_nc_ht=scontent.fhan14-1.fna&oh=cf8c39e7fc855beb3b9f75ab79c6d08c&oe=61B23952"
-    },
-    {
-      name: "Độ Mixi",
-      img: "https://scontent.fhan14-2.fna.fbcdn.net/v/t1.6435-9/151296956_2198779476922608_9064820515933100826_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=174925&_nc_ohc=TzrZGvBVkBkAX_IeMqu&tn=d5LeRVdLTryOw5MC&_nc_ht=scontent.fhan14-2.fna&oh=438b541d1007f515e7cf69a82218b728&oe=61B4FBE2"
-    },
-    {
-      name: "La Phu",
-      img: "https://www.iptc.org/wp-content/uploads/2018/05/avatar-anonymous-300x300.png"
-    },
+//   ]
 
-  ]
-
-  const list2 = [
-    {
-      name: "Nguyễn Thế Tường",
-      img: "https://scontent.fhan3-2.fna.fbcdn.net/v/t39.30808-6/241413022_125192916590605_6458598303746510986_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=9WNM9OkJqmsAX-1l5yF&_nc_ht=scontent.fhan3-2.fna&oh=0253efd8cbe179f11b4a3f73cad66186&oe=6197F515",
-    },
-    {
-      name: "Thủy Quân",
-      img: "https://scontent.fhan3-3.fna.fbcdn.net/v/t39.30808-6/257268411_125828893180209_3382620192621732002_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=HNolMZ_rlCMAX_tPMMt&_nc_ht=scontent.fhan3-3.fna&oh=0add518a881fa6c0b2d3522bca9fcab8&oe=6197856F"
-    },
-    {
-      name: "Tlinh",
-      img: "https://scontent.fhan4-3.fna.fbcdn.net/v/t1.6435-1/p320x320/159867274_113114454174570_5891848019290317339_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=7206a8&_nc_ohc=zIUtXA__lmwAX9l-sWc&tn=d5LeRVdLTryOw5MC&_nc_ht=scontent.fhan4-3.fna&oh=bdb478f0a2e807dfbfe52e8cccba40e6&oe=61B7E7BE"
-    },
-    {
-      name: "Nhâm Hoàng Kah",
-      img: "https://scontent.fhan3-5.fna.fbcdn.net/v/t1.6435-9/110094262_111070337356179_6141206220175576581_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=3RwbkJ2bTiIAX9yDgGJ&_nc_ht=scontent.fhan3-5.fna&oh=d98b3f602a2da696c89081441f1d09ce&oe=61B9D66E"
-    },
-    {
-      name: "Bác Môn",
-      img: "https://scontent.fhan3-3.fna.fbcdn.net/v/t1.6435-1/s320x320/161210699_102388895276850_5562154680033495070_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=7206a8&_nc_ohc=cIPHIqzehiwAX8HX1ld&_nc_ht=scontent.fhan3-3.fna&oh=06bc7399bf6011f44237f2da2d3fb015&oe=61B6DBCC"
-    },
-    {
-      name: "Ngọc Anh",
-      img: "https://scontent.fhan3-5.fna.fbcdn.net/v/t39.30808-6/241182521_400257455052760_2825545721688670223_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=nhOTFLQO-4EAX8DeL9W&_nc_ht=scontent.fhan3-5.fna&oh=c2632077a6ba2bdfe7aef0a7eb18c795&oe=61975977"
-    },
-    {
-      name: "Ngô Trang",
-      img: "https://scontent.fhan4-3.fna.fbcdn.net/v/t1.6435-1/c0.107.320.320a/p320x320/162192495_179876320609187_4096475874471244693_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=7206a8&_nc_ohc=cIcsbF1g600AX_Rp2I7&_nc_ht=scontent.fhan4-3.fna&oh=8c443f383b730aae8575a4991f7e8315&oe=61B8ECA7"
-    },
-    {
-      name: "Long",
-      img: "https://scontent.fhan3-4.fna.fbcdn.net/v/t39.30808-1/p320x320/242189980_581501463285447_5502115508154574854_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=7206a8&_nc_ohc=2OrmMmms9E8AX-O5hbP&tn=d5LeRVdLTryOw5MC&_nc_ht=scontent.fhan3-4.fna&oh=ea0c4c6e8759ab0f2eac3e8c54588280&oe=61973783"
-    },
-    {
-      name: "TITAN",
-      img: "https://scontent.fhan3-5.fna.fbcdn.net/v/t1.6435-1/p320x320/202205921_1888584467977661_6537798788753969703_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=7206a8&_nc_ohc=8Cf0pUQhr3cAX8irKQn&_nc_oc=AQlAECnRgxpFF_iDtcOqOe07c2N5wBg5clOAwkGqyhHUXuXYJ3zvsnAVF-vY-Rnw84k&_nc_ht=scontent.fhan3-5.fna&oh=e3e3c9b935a4b45cddeac1aa4e2e3b3e&oe=61B894E0"
-    },
-  ]
+//   const list2 = [
+//     {
+//       name: "Nguyễn Thế Tường",
+//       img: "https://scontent.fhan3-2.fna.fbcdn.net/v/t39.30808-6/241413022_125192916590605_6458598303746510986_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=9WNM9OkJqmsAX-1l5yF&_nc_ht=scontent.fhan3-2.fna&oh=0253efd8cbe179f11b4a3f73cad66186&oe=6197F515",
+//     },
+//     {
+//       name: "Thủy Quân",
+//       img: "https://scontent.fhan3-3.fna.fbcdn.net/v/t39.30808-6/257268411_125828893180209_3382620192621732002_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=HNolMZ_rlCMAX_tPMMt&_nc_ht=scontent.fhan3-3.fna&oh=0add518a881fa6c0b2d3522bca9fcab8&oe=6197856F"
+//     },
+//     {
+//       name: "Tlinh",
+//       img: "https://scontent.fhan4-3.fna.fbcdn.net/v/t1.6435-1/p320x320/159867274_113114454174570_5891848019290317339_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=7206a8&_nc_ohc=zIUtXA__lmwAX9l-sWc&tn=d5LeRVdLTryOw5MC&_nc_ht=scontent.fhan4-3.fna&oh=bdb478f0a2e807dfbfe52e8cccba40e6&oe=61B7E7BE"
+//     },
+//     {
+//       name: "Nhâm Hoàng Kah",
+//       img: "https://scontent.fhan3-5.fna.fbcdn.net/v/t1.6435-9/110094262_111070337356179_6141206220175576581_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=3RwbkJ2bTiIAX9yDgGJ&_nc_ht=scontent.fhan3-5.fna&oh=d98b3f602a2da696c89081441f1d09ce&oe=61B9D66E"
+//     },
+//     {
+//       name: "Bác Môn",
+//       img: "https://scontent.fhan3-3.fna.fbcdn.net/v/t1.6435-1/s320x320/161210699_102388895276850_5562154680033495070_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=7206a8&_nc_ohc=cIPHIqzehiwAX8HX1ld&_nc_ht=scontent.fhan3-3.fna&oh=06bc7399bf6011f44237f2da2d3fb015&oe=61B6DBCC"
+//     },
+//     {
+//       name: "Ngọc Anh",
+//       img: "https://scontent.fhan3-5.fna.fbcdn.net/v/t39.30808-6/241182521_400257455052760_2825545721688670223_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=nhOTFLQO-4EAX8DeL9W&_nc_ht=scontent.fhan3-5.fna&oh=c2632077a6ba2bdfe7aef0a7eb18c795&oe=61975977"
+//     },
+//     {
+//       name: "Ngô Trang",
+//       img: "https://scontent.fhan4-3.fna.fbcdn.net/v/t1.6435-1/c0.107.320.320a/p320x320/162192495_179876320609187_4096475874471244693_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=7206a8&_nc_ohc=cIcsbF1g600AX_Rp2I7&_nc_ht=scontent.fhan4-3.fna&oh=8c443f383b730aae8575a4991f7e8315&oe=61B8ECA7"
+//     },
+//     {
+//       name: "Long",
+//       img: "https://scontent.fhan3-4.fna.fbcdn.net/v/t39.30808-1/p320x320/242189980_581501463285447_5502115508154574854_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=7206a8&_nc_ohc=2OrmMmms9E8AX-O5hbP&tn=d5LeRVdLTryOw5MC&_nc_ht=scontent.fhan3-4.fna&oh=ea0c4c6e8759ab0f2eac3e8c54588280&oe=61973783"
+//     },
+//     {
+//       name: "TITAN",
+//       img: "https://scontent.fhan3-5.fna.fbcdn.net/v/t1.6435-1/p320x320/202205921_1888584467977661_6537798788753969703_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=7206a8&_nc_ohc=8Cf0pUQhr3cAX8irKQn&_nc_oc=AQlAECnRgxpFF_iDtcOqOe07c2N5wBg5clOAwkGqyhHUXuXYJ3zvsnAVF-vY-Rnw84k&_nc_ht=scontent.fhan3-5.fna&oh=e3e3c9b935a4b45cddeac1aa4e2e3b3e&oe=61B894E0"
+//     },
+//   ]
 
 
 
@@ -203,14 +240,13 @@ export default function App() {
 
 
   let tmp = [];
-  for (let i = 0; i < list1.length; i++) {
-    tmp.push(<FriendReceived key={i} name={list1[i].name} img={list1[i].img} />);
+  for (let i = 0; i < ListS.length; i++) {
+    tmp.push(<FriendReceived key={i} name={ListS[i].receiver.username} img={BaseURL+ListS[i].receiver.avatar.fileName} />);
   }
   let tmp1 = [];
-  for (let i = 0; i < list2.length; i++) {
-    tmp1.push(<FriendSent key={i} name={list2[i].name} img={list2[i].img} />);
+  for (let i = 0; i < ListR.length; i++) {
+    tmp1.push(<FriendSent key={i} name={ListR[i].sender.username} img={BaseURL+ListR[i].sender.avatar.fileName} />);
   }
-
 
 function ListSent() {
   return (
@@ -247,14 +283,14 @@ function MyTabs (){
       <Tab.Screen
         name="ListReceived"
         component={ListReceived}
-        options={{ tabBarLabel: 'ĐÃ NHẬN ' + list1.length}}
+        options={{ tabBarLabel: 'ĐÃ NHẬN ' + ListR.length}}
         
         
       />
       <Tab.Screen
         name="ListSent"
         component={ListSent}
-        options={{ tabBarLabel: 'ĐÃ GỬI ' + list1.length}}
+        options={{ tabBarLabel: 'ĐÃ GỬI ' + ListS.length}}
       />
       
     </Tab.Navigator>
